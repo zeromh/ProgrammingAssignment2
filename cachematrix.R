e15e9d336cd6ede033b73f5c7b74686ec11ec4b1
zeromh <zeromh@gmail.com>
2014-06-21 22:43:23 -0400

    Modified functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow a user to compute the inverse of
+## a matrix and cache the result, so that if the result is
+## needed later, it will not be re-computed, but simply retrieved
+## from the cache.
 
-## Write a short comment describing this function
+## This function returns a list of functions that can store or
+## retrieve the matrix whose inverse is to be computed. The functions 
+## in the list can also compute and retreive said inverse. The
+## matrix and its inverse are stored in a variable located inside
+## the function's environment, so it cannot be affected by
+## variable manipulations in the global environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes as its argument the list of functions returned
+## in the previous function. Using those functions, it computes and
+## retrieves a matrix's inverse. If the inverse
+## has already been computed, this function retrieves that saved value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()   ## Return a matrix that is the inverse of 'x'
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
